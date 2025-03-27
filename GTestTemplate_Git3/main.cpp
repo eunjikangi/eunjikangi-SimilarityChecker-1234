9a59760c998ccd1d0db751eb0dca9c527a5f970b
eunjikangi <miki2323@naver.com>
2025-03-28 02:21:01 +0900

    [feature] alpha

--- GTestTemplate_Git3/main.cpp
+++ GTestTemplate_Git3/main.cpp
@@ -7,24 +7,43 @@ class SimilarityCheckerFixture : public testing::Test {
 public:
 	SimilarityChecker similarityChecker;
 
-	void checkString(string str1, string str2, int expected) {
+	void checkStringLen(string str1, string str2, int expected) {
 		EXPECT_EQ(expected, similarityChecker.lengthCheck(str1, str2));
+	}
+
+	void checkStringAlpha(string str1, string str2, int expected) {
+		EXPECT_EQ(expected, similarityChecker.alphaCheck(str1, str2));
 	}
 };
 
 TEST_F(SimilarityCheckerFixture, StringNum1)
 {
-	checkString("ASD", "DSA", 60);
+	checkStringLen("ASD", "DSA", 60);
 }
 
 TEST_F(SimilarityCheckerFixture, StringNum2)
 {
-	checkString("A", "BB", 0);
+	checkStringLen("A", "BB", 0);
 }
 
 TEST_F(SimilarityCheckerFixture, StringNum3)
 {
-	checkString("AAABB", "BAA", 20);
+	checkStringLen("AAABB", "BAA", 20);
+}
+
+TEST_F(SimilarityCheckerFixture, StringNum4)
+{
+	checkStringLen("AA", "AAE", 30);
+}
+
+TEST_F(SimilarityCheckerFixture, AlphaCheck1)
+{
+	checkStringAlpha("ASD", "DSA", 40);
+}
+
+TEST_F(SimilarityCheckerFixture, AlphaCheck2)
+{
+	checkStringAlpha("A", "BB", 0);
 }
 
 int main() {
--- GTestTemplate_Git3/similarityChecker.cpp
+++ GTestTemplate_Git3/similarityChecker.cpp
@@ -24,4 +24,26 @@ public:
         return result;
 	}
 
+    int alphaCheck(string str1, string str2) {
+        int sameCount = 0;
+        int totalCount = 0;
+
+        for (int s1 = 0; s1 < str1.length(); ++s1) {
+            bool isSame = false;
+            for (int s2 = 0; s2 < str2.length(); ++s2) {
+                if (str1[s1] == str2[s2]) {
+                    isSame = true;
+                    break;
+                }
+            }
+
+            if (isSame == true) {
+                sameCount++;
+            }
+
+            totalCount++;
+        }
+
+        return ((double)sameCount / (double)totalCount) * 40;
+    }
 };
